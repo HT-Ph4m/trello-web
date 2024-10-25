a253e4309912b2dca2437d762ea87a2279969dd3
Pham Hong Truong <truong.pham@adnx.vn>
2024-10-25 15:33:36 +0700

    app bar, board bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
@@ -8,6 +7,8 @@ import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import theme from './theme'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -17,62 +18,78 @@ function ModeSelect() {
     setMode(mode)
   }
   return (
-    <Box sx={{ m: 2, minWidth: 120 }}>
-      <FormControl size="small">
-        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-        <Select
-          labelId="label-select-dark-light-mode"
-          id="select-dark-light-mode"
-          value={mode}
-          label="Mode"
-          onChange={handleChange}
-        >
-          <MenuItem value="light">
-            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <LightModeIcon fontSize="small" />
-              Light
-            </div>
-          </MenuItem>
-          <MenuItem value="dark">
-            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <DarkModeIcon fontSize="small" />
-              Dark
-            </div>
-          </MenuItem>
-          <MenuItem value="system">
-            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <Brightness4Icon fontSize="small" />
-              System
-            </div>
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  )
-}
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Brightness4Icon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <h1>Hello, Vite!</h1>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
+    <Container disableGutters maxWidth={false}>
+      {/* app Bar */}
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      {/* Boar Bar */}
+      <Box
+        sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: (theme) => theme.trello.boardBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Main
+      </Box>
+    </Container>
   )
 }
 
